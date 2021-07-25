3359600188ac1843fbcaa54187cf27fdade86b5e
Michael Zimmerman <mzimme1290@gmail.com>
2021-07-25 18:47:23 -0500

    Add task.py and place holder code for tests

---
+++ task.py
@@ -0,0 +1,18 @@
+def conv_num(num_str):
+	'''
+	This function takes in a string and converts it into a base 10 number
+	and returns it.
+	'''
+	return 12345
+
+
+def my_datetime(num_sec):
+	'''
+	'''
+	return "01-01-1970"
+
+
+def conv_endian(num, endian):
+	'''
+	'''
+	return "0E 91 A2"
--- tests.py
+++ tests.py
@@ -1,10 +1,25 @@
 import unittest
+from task import conv_num
+from task import my_datetime
+from task import conv_endian
 
 
-class TestCase(unittest.TestCase):
+class Test_conv_num(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    def test_conv_num_positive_int(self):
+        self.assertEqual(conv_num("12345"), 12345)
+
+
+class Test_my_datetime(unittest.TestCase):
+
+    def test_zero(self):
+        self.assertEqual(my_datetime(0), "01-01-1970")
+
+
+class Test_conv_endian(unittest.TestCase):
+
+    def test_big(self):
+        self.assertEqual(conv_endian(954786, "big"), "0E 91 A2")
 
 
 if __name__ == '__main__':
