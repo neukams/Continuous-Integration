e7ebcbd7b5432360bcf5a15e1ac82b20a4a251e3
Dustin Walkup <walkupd@oregonstate.edu>
2021-08-03 18:33:55 -0700

    Add conv_endian (#3)
    * wrote test1 to see if num returns hex number
    
    * implemented int_to_hex func to convert integers to hexadecimal, added this implementation to conv_endian to pass test1
    
    * wrote TestConvEndian test2 to check for negative input, test fails
    
    * Implemented TestConvEndian to handle negative integers
    
    * Wrote TestConvEndian test3 to return little endian hex number
    
    * Implemented conv_endian to handled little endian parameter
    
    * wrote TestConvEndian test4 to test for invalid endian types
    
    * commented out test4
    
    * added .idea/ to .gitignore
    
    * Delete .idea directory
    
    * Implemented conv_endian to handle invalid endian types
    
    * Added TestConvEndian random testing and edge cases tests 5-9
    
    * added comments
    
    * updated with recommended changes from PR
    
    * fixed merge conflicts

--- task.py
+++ task.py
@@ -15,11 +15,90 @@ def my_datetime(num_sec):
     return "01-01-1970"
 
 
-def conv_endian(num, endian):
-    '''
-    Pending
-    '''
-    return "0E 91 A2"
+def conv_endian(num, endian='big'):
+    """
+    accepts integer and endian type and returns a hexadcimal string
+    """
+    endian = endian.lower()
+
+    # if the endian value is something other than big or little, return none
+    if (endian != 'big') and (endian != 'little'):
+        return None
+
+    negative = False
+
+    # if integer is negative, make it positive set negative boolean to True
+    if num < 0:
+        num *= -1
+        negative = True
+
+    # calls function that returns hexadecimal number string
+    hex_string = int_to_hex(num)
+
+    # if endian is little, call conv_little_endian function to convert from big
+    # to little endian
+    if endian == 'little':
+        hex_string = conv_little_endian(hex_string)
+
+    # strip string of any additional whitespace
+    hex_string = hex_string.strip()
+
+    # if integer was negative, add negative sign to hex number string
+    if negative:
+        hex_string = '-' + hex_string
+
+    return hex_string
+
+
+def int_to_hex(num):
+    """
+    accepts integer returns hex number string with two character bytes spaces
+    """
+    if num == 0:
+        return '00'
+
+    # dictionary that will convert digits to hex number
+    hex_dic = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7',
+               8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E',
+               15: 'F'}
+
+    hex_string = ''
+    space_count = 0
+    char_count = 0
+
+    # converts each digit to its hex counterpart, concatenates it to hex_string
+    while num > 0:
+        remainder = num % 16
+        hex_char = hex_dic.get(remainder)
+        hex_string = hex_char + hex_string
+        space_count += 1
+        char_count += 1
+
+        # adds a space every 2 chars
+        if space_count == 2:
+            hex_string = ' ' + hex_string
+            space_count = 0
+
+        num = num // 16
+
+    # if hex string digits are odd, add a zero to beginning of string
+    if char_count % 2 != 0:
+        hex_string = '0' + hex_string
+
+    return hex_string
+
+
+def conv_little_endian(hex_string):
+    """
+    converts big endian to little endian hex number
+    """
+    temp_string = ''
+
+    while hex_string != '':
+        last_two = hex_string[-2:]
+        temp_string = temp_string + last_two + " "
+        hex_string = hex_string[:-3]
+    return temp_string
 
 
 class Number:
--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ import unittest
 from task import conv_num
 from task import my_datetime
 from task import conv_endian
+import random
 
 
 class TestConvNum(unittest.TestCase):
@@ -120,15 +121,61 @@ class TestMyDatetime(unittest.TestCase):
 
 
 class TestConvEndian(unittest.TestCase):
-    '''
-    Pending
-    '''
 
-    def test_big(self):
-        '''
-        Pending
-        '''
-        self.assertEqual(conv_endian(954786, "big"), "0E 91 A2")
+    def test1(self):
+        var = 954786
+        expected = "0E 91 A2"
+        self.assertEqual(conv_endian(var), expected)
+
+    def test2(self):
+        var = -954786
+        expected = "-0E 91 A2"
+        self.assertEqual(conv_endian(var), expected)
+
+    def test3(self):
+        var = 954786
+        expected = "A2 91 0E"
+        self.assertEqual(conv_endian(var, 'little'), expected)
+
+    def test4(self):
+        var = 954786
+        expected = None
+        self.assertEqual(conv_endian(var, 'small'), expected)
+
+    # random testing
+    def test5(self):
+        min_int = -2147483648
+        max_int = 2147483647
+        for i in range(0, 500):
+            endian_value = random.randint(0, 1)
+            endian_value = 'big' if endian_value == 1 else 'little'
+            var = random.randint(min_int, max_int)
+            conv_endian(var, endian_value)
+
+    def test6(self):
+        var = 14
+        expected = '0E'
+        self.assertEqual(conv_endian(var, 'little'), expected)
+
+    def test7(self):
+        var = 0
+        expected = '00'
+        self.assertEqual(conv_endian(var), expected)
+
+    def test8(self):
+        var = 2147483647
+        expected = '7F FF FF FF'
+        self.assertEqual(conv_endian(var), expected)
+
+    def test9(self):
+        var = -2147483648
+        expected = '-00 00 00 80'
+        self.assertEqual(conv_endian(var, 'little'), expected)
+
+    def test10(self):
+        var = 954786
+        expected = "A2 91 0E"
+        self.assertEqual(conv_endian(var, 'LitTle'), expected)
 
 
 if __name__ == '__main__':
